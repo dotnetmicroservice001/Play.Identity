2bc449154c53dee9f39cf4921c3df44ae94ebfa0
basnets24 <sneha.basnet2@gmail.com>
2025-06-03 00:18:30 -0400

    seeding roles

--- src/Play.Identity.Service/HostedServices/IdentitySeedHostedService.cs
+++ src/Play.Identity.Service/HostedServices/IdentitySeedHostedService.cs
@@ -24,15 +24,33 @@ public class IdentitySeedHostedService : IHostedService
     }
     
     
-    public Task StartAsync(CancellationToken cancellationToken)
+    public async Task StartAsync(CancellationToken cancellationToken)
     {
-        throw new System.NotImplementedException();
+        using var scope = _serviceScopeFactory.CreateScope();
+
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>(); 
+        
+        await CreateRoleIfNotExistsAsync(Roles.Admin, roleManager);
+        await CreateRoleIfNotExistsAsync(Roles.Player, roleManager);
+
+        var adminUser = await userManager.FindByEmailAsync(_settings.AdminUserEmail);
+
+        if (adminUser == null)
+        {
+            adminUser = new ApplicationUser
+            {
+                UserName = _settings.AdminUserEmail,
+                Email = _settings.AdminUserEmail,
+            };
+            
+            await userManager.CreateAsync(adminUser, _settings.AdminUserPassword);
+            await userManager.AddToRoleAsync(adminUser, Roles.Admin);
+            
+        }
     }
 
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        throw new System.NotImplementedException();
-    }
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 
     private static async Task CreateRoleIfNotExistsAsync(string role, 
         RoleManager<ApplicationRole> roleManager)
