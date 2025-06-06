b0b7344d82528795b4b1607ea39678c65c1df8f5
basnets24 <sneha.basnet2@gmail.com>
2025-06-06 00:55:15 -0400

    role based authorization

--- src/Play.Identity.Service/Controllers/UsersController.cs
+++ src/Play.Identity.Service/Controllers/UsersController.cs
@@ -12,7 +12,7 @@ namespace Play.Identity.Service.Controllers;
 
 [ApiController]
 [Route("users")]
-[Authorize( Policy = IdentityServerConstants.LocalApi.PolicyName)]
+[Authorize( Policy = IdentityServerConstants.LocalApi.PolicyName, Roles = Roles.Admin)]
 public class UsersController : ControllerBase
 {
     private readonly UserManager<ApplicationUser> _userManager;
