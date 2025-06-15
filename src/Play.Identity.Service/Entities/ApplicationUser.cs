991b015b8263f6796b0a8e4826d952b0c5f4a464
basnets24 <sneha.basnet2@gmail.com>
2025-06-15 18:34:50 -0400

    idempotent consumers

--- src/Play.Identity.Service/Entities/ApplicationUser.cs
+++ src/Play.Identity.Service/Entities/ApplicationUser.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using AspNetCore.Identity.MongoDbCore.Models;
 using MongoDbGenericRepository.Attributes;
 
@@ -8,4 +9,6 @@ namespace Play.Identity.Service.Entities;
 public class ApplicationUser : MongoIdentityUser<Guid>
 {
     public decimal Gil { get; set; }
+    
+    public HashSet<Guid> MessageIds { get; set; } = new HashSet<Guid>();
 }
