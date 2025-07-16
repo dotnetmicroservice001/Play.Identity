206b9220ec954e51f8e0057e9d69961b12b87cf7
basnets24 <sneha.basnet2@gmail.com>
2025-07-16 12:32:16 -0400

    used addTracing from common lib

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -17,6 +17,7 @@ using MongoDB.Bson.Serialization.Serializers;
 using Play.Common.HealthChecks;
 using Play.Common.Logging;
 using Play.Common.MassTransit;
+using Play.Common.OpenTelemetry;
 using Play.Common.Settings;
 using Play.Identity.Service.Entities;
 using Play.Identity.Service.Exception;
@@ -55,7 +56,8 @@ namespace Play.Identity.Service
                     mongoDbSettings.ConnectionString,
                     serviceSettings.ServiceName
                     );
-            services.AddSeqLogging(Configuration);
+            services.AddSeqLogging(Configuration)
+                .AddTracing(Configuration);
             
             services.AddMassTransitWithMessageBroker(Configuration, retryConfigurator =>
             {
